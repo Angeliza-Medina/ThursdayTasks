5d15603a86063d64b4a0fc65927a643d4722ed25
Angeliza-Medina <78863493+Angeliza-Medina@users.noreply.github.com>
2021-02-24 16:34:04 +0100

    Finish task 1

---
+++ Week 8/TaskOne/Main.java
@@ -0,0 +1,23 @@
+
+class Main{
+
+  public static void main(String[] args){
+
+    printEmptyLine();
+    printString("My name is Angeliza");
+    printNameAndAge("Angeliza", 25);
+  }
+
+  public static void printEmptyLine(){
+    System.out.println("");
+  }
+
+  public static void printString(String yourString){
+    System.out.println(yourString);
+  }
+
+  public static void printNameAndAge(String name, int age){
+    System.out.println("My name is " + name + ", I am " + age + " years old.");
+  }
+
+}
